bbc2d4ab5604125c35e3682d306fc4fe62629a57
Fabio Hsu <Fabio90h@fabios-mbp.attlocal.net>
2019-09-09 09:42:50 -0700

    Displaying the latest 6 fixtures

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import SelectTeam from './SelectTeam'
+//import Fixtures from './Fixtures'
 import axios from 'axios'
+import FixtureCard from './FixtureCard'
+
 
 const Home = () => {
     const [teamFixtures, setTeamFixtures] = useState("")
@@ -15,9 +18,25 @@ const Home = () => {
         console.log(res.data)
     }
     const handleButtonFixtures = () => {
-        console.log(teamFixtures)
+        console.log(printFixtures())
     }
 
+    // SHOW ONLY 6 FIXTURES. 3 FROM THE RESULT AND 3 FROM SCHEDULED
+    const printFixtures = () => {
+        if(teamFixtures.length < 1) return []
+        // FINDING THE NEXT SCHEDULED FIXTURE
+        let currentFixtureIndex = teamFixtures.findIndex(current => current.status === "SCHEDULED")
+        // GOING BACK TO FINISHED FIXTURES
+        let finishedFixtureIndex = currentFixtureIndex - 3
+        let fixturesArray = []
+        
+        for(let fixtureIndex = finishedFixtureIndex; fixtureIndex < finishedFixtureIndex + 6; fixtureIndex++){
+            fixturesArray.push(
+                <FixtureCard key={fixtureIndex} fixtureData={teamFixtures[fixtureIndex]}/>
+            )
+        }
+        return fixturesArray
+    }
 
 
     return(
@@ -25,9 +44,9 @@ const Home = () => {
             <div>
                 <SelectTeam setTeamFixturesParent={setTeamFixtures}/>
             </div>
-            <div>
-
-            </div>
+            {
+                printFixtures()
+            }
             <button onClick={handleButtonTest}>tests</button>
             <button onClick={handleButtonSearch}>search</button>
             <button onClick={handleButtonFixtures}>fixtures</button>
